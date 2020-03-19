fa082ca06f9631d0b6dd730999c86417df77c836
Andrik-Ts-019 <andrikdlcmartinez@outlook.com>
2020-03-19 14:10:34 -0600

    Cambio del tipo de libreria Controller y Model a dll

--- 113 EA2 E7/principalProgram.cs
+++ 113 EA2 E7/principalProgram.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Controller;
 
 namespace WindowsFormsApp1
 {
@@ -14,6 +13,7 @@ namespace WindowsFormsApp1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new entrada());
             Application.Run(new principal());
         }
     }
--- Controller/Class1.cs
+++ Controller/Class1.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Model
+namespace Controller
 {
-    public class Class2
+    public class Class1
     {
     }
 }
