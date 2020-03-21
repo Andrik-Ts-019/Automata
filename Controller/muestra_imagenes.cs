fa11d7368d98ec42c0aee496b188980820b195c3
Andrik-Ts-019 <andrikdlcmartinez@outlook.com>
2020-03-21 01:41:55 -0600

    se modifico el model y controller

--- Controller/muestra_imagenes.cs
+++ Controller/muestra_imagenes.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using System.Drawing;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Model;
 
 namespace Controller
 {
-    public class Class1
+    public class muestra_imagenes
     {
+        createImg nuevaimg = new createImg();
+        string nom;
     }
 }
--- Model/Llama_imagenes.cs
+++ Model/Llama_imagenes.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Drawing;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +8,18 @@ namespace Model
 {
     public class createImg
     {
+        public Image imagenprueba
+        {
+            set { this.imagenprueba = Images.img_test; }
+
+            get { return imagenprueba; }
+        }
+
+        public string nombre
+        {
+            set { this.nombre = "Andrik"; }
+
+            get { return nombre; }
+        }
     }
 }
